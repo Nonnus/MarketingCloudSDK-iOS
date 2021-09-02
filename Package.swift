fec802f76b92a99040a75ef55b4b8304c03251cb
Nonnus <np@hellofresh.com>
2021-09-02 21:09:12 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,13 +1,11 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "MarketingCloudSDK",
     platforms: [.iOS(.v10)],
-    products: [.library(name: "MarketingCloudSDK",
-                        targets: ["MarketingCloudSDK"])],
-    targets: [.binaryTarget(name: "MarketingCloudSDK",
-                            path: "MarketingCloudSDK.xcframework")]
+    products: [.library(name: "MarketingCloudSDK", targets: ["MarketingCloudSDK"])],
+    targets: [.binaryTarget(name: "MarketingCloudSDK", path: "MarketingCloudSDK/MarketingCloudSDK.xcframework")]
+//    resources: [.process("MarketingCloudSDK/MarketingCloudSDK.bundle")])
 )
